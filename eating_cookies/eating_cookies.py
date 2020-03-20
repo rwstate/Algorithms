c76c4aff917d214253989312816dd9c048ef3db2
Ryan State <rwstate@gmail.com>
2020-03-20 10:03:29 -0400

    add memoization to eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,19 +4,27 @@ import sys
 
 # base cases n < 2 return 1, n< 0 return 0
 
+def cookie_helper(n, index, cache):
+  cache[index] += (cache[index - 3] + cache[index - 2] + cache [index - 1])
+  if index == n:
+    return cache
+  return cookie_helper(n, index + 1, cache)
+
 def eating_cookies(n, cache=None):
-  if n < 0:
-    return 0
-  if n == 0:
-    return 1
-  return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+  if n == 0: return 1
+  if n == 1: return 1
+  initCache = [0] * (n + 1)
+  initCache[0] = 1
+  initCache[1] = 1
+  return cookie_helper(n, 2, initCache)[-1]
 
 print(eating_cookies(10))
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
--- making_change/making_change.py
+++ making_change/making_change.py
@@ -31,12 +31,12 @@ def making_change(amount, denominations):
 
 print(making_change(200, [1, 5, 10, 25, 50]))
 
-# if __name__ == "__main__":
-#   # Test our your implementation from the command line
-#   # with `python making_change.py [amount]` with different amounts
-#   if len(sys.argv) > 1:
-#     denominations = [1, 5, 10, 25, 50]
-#     amount = int(sys.argv[1])
-#     print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-#   else:
-#     print("Usage: making_change.py [amount]")
+if __name__ == "__main__":
+  # Test our your implementation from the command line
+  # with `python making_change.py [amount]` with different amounts
+  if len(sys.argv) > 1:
+    denominations = [1, 5, 10, 25, 50]
+    amount = int(sys.argv[1])
+    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+  else:
+    print("Usage: making_change.py [amount]")
